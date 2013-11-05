9b02a4a33430db14d2231bf2c707b26fd709065e
Aymen Ibrahim <aymen_ibrahim@hotmail.com>
2013-11-05 19:24:59 +0300

    Alhamdullilah, FloodFill finally works ! 19:24, nov , 5

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import cv2 as cv
 import numpy as np
 import random
 
-im = cv.imread('test_image_3.jpg')
+im = cv.imread('test_image_4.jpg')
 h , w = im.shape[:2]
 # print im.shape
 # cap = cv.VideoCapture(1)
@@ -26,10 +26,10 @@ element = cv.getStructuringElement(cv.MORPH_RECT,(17,3))
 
 gray = cv.morphologyEx(gray,cv.MORPH_CLOSE ,element)
 
-contours ,hierarchy = cv.findContours(gray,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
+contours ,hierarchy = cv.findContours(gray.copy(),cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
 
 def verify_sizes(mr):
-	print mr
+	# print mr
 	error = 0.4
 	#TODO aspect ratio of the plate for Ethiopia 
 	aspect = 4.7272
@@ -47,7 +47,7 @@ def verify_sizes(mr):
 		r = mr[1][0] / mr[1][1]
 	except:
 		return False
-	print "r" , r
+	# print "r" , r
 	if (r<1):
 		r = 1/r
 
@@ -60,7 +60,7 @@ def verify_sizes(mr):
 rects = []
 for cont in contours:
 	mr = cv.minAreaRect(cont)
-	if not verify_sizes(mr):
+	if  verify_sizes(mr):
 		# print mr
 		rects.append(cont)
 
@@ -68,38 +68,43 @@ for cont in contours:
 
 cv.drawContours(im,rects,-1,(0,0,255))
 
+fixed_range = True
+connectivity = 4
+lo = 38
+hi = 39
+mask = np.zeros((h+2, w+2), np.uint8)
+mask[:] = 0
+flags = connectivity #+  cv.FLOODFILL_FIXED_RANGE  +  
+flags |=cv.FLOODFILL_MASK_ONLY
+flags |= cv.FLOODFILL_FIXED_RANGE
+flags |= (255 << 8)
 for rect in rects:
 	mr = cv.minAreaRect(rect)
-	print "mr >>", mr
+	# print "mr >>", mr
 	# cv.circle(im,(int(mr[0][1]) , int(mr[0][0])),50,(0,255,0))
 	minSize = min(mr[0])
-	minSize = int(minSize - minSize*0.5)
+	minSize = minSize - minSize*0.5
 	# random.seed()
-	mask = np.zeros((h+2, w+2), np.uint8)
 	seed_pt = (int(mr[0][0]) , int(mr[0][1]))
-	fixed_range = True
-	connectivity = 4
-	lo = 39
-	hi = 38
-	newMaskVal = 255
-	numSeeds = 10
-	mask[:] = 0
-	flags = connectivity #+ cv.FLOODFILL_FIXED_RANGE + cv.FLOODFILL_MASK_ONLY + (newMaskVal << 8)
-	flags |= cv.FLOODFILL_FIXED_RANGE
-	# for x in xrange(numSeeds):
-	rand_pt = seed_pt #(int( seed_pt[0] + random.random() * minSize)  ,int( seed_pt[1] + random.random() * minSize) )
-	print "<><><><>>> random",rand_pt
-	try:
-		cv.circle(im, rand_pt, 2, (0,  255 ,0), -1)
-		cv.floodFill(im, mask, rand_pt, (255, 255, 255), (lo,)*3, (hi,)*3, flags)
-	except :
-		pass
-# cv.drawContours(im,contours,-1,(255,0,0)) #blue
-cv.drawContours(im,rects,-1,(0,0,255)) #red
+	print "seed_pt" , seed_pt
+	for x in xrange(10):
+		rand_pt =  (int(mr[0][0] + random.random() % minSize - (minSize/2)) , int(mr[0][1]+ random.random() % minSize- (minSize/2)))
+		print "r",rand_pt
+		try:
+			cv.floodFill(im, mask, rand_pt, (255, 255, 255), (lo,)*3, (hi,)*3, flags)
+			cv.circle(im, rand_pt, 2, (0, 0, 255), -1)
+		except :
+			print "something wrong!"
 
+
+# cv.drawContours(mask,rects,-1,(255,0,255)) #red
+
+# contours ,hierarchy = cv.findContours(mask,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
+# cv.drawContours(mask,contours,-1,(255,0,0)) #blue
 
 
 cv.imshow('test',im)
+cv.imshow('mask',mask)
 0xFF & cv.waitKey()#(10)
 
 
